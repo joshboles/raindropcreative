f5020239ad08144dda8064de85b142874ec673d9
Issac Kelly <issac.kelly@gmail.com>
2010-10-26 17:30:17 -0400

    Update deploy info

---
+++ deploy/cron.sh
@@ -0,0 +1,2 @@
+cd /home/serveruser/development-sites/raindropcreative
+git pull -Xtheirs origin master
---
+++ deploy/gunicorn.conf.py
@@ -0,0 +1,3 @@
+bind = "127.0.0.1:7001"
+logfile = "/home/serveruser/logs/raindropcreative/gunicorn.log"
+workers = 3
