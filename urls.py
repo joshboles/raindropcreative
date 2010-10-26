13c3c6c1a6d0f38ccda6d820fef6456b673a7618
Issac Kelly <issac.kelly@gmail.com>
2010-10-26 17:22:07 -0400

    First Commit

---
+++ urls.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+
+from django.contrib import admin
+admin.autodiscover()
+
+from pinax.apps.account.openid_consumer import PinaxConsumer
+
+
+handler500 = "pinax.views.server_error"
+
+
+urlpatterns = patterns("",
+    url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^account/", include("pinax.apps.account.urls")),
+    url(r"^servee/", include("servee.urls")),
+    url(r"^frontendadmin/", include("frontendadmin.urls")),
+    url(r"^openid/(.*)", PinaxConsumer()),
+)
+
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns("",
+        url(r"", include("staticfiles.urls")),
+    )
