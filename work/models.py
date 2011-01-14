887337b3e74197daabfec504eb95faa85f5a309c
Josh Boles <jaboles@gmail.com>
2011-01-14 14:40:25 -0500

    Wrote work app model

--- work/models.py
+++ work/models.py
@@ -3,5 +3,19 @@ from taggit.managers import TaggableManager
 import datetime
 
 # Create your models here.
-class Projects(models.Model):
-    tags = TaggableManager
+class Project(models.Model):
+    name = models.CharField(max_length=64)
+    order = models.IntegerField()
+    active = models.BooleanField()
+    
+    def __unicode__(self):
+      return self.name
+
+class Photo(models.Model):
+    project = models.ForeignKey(Project, related_name="photo_set")
+    description = models.TextField(blank=True, null=True)
+    file = models.FileField(upload_to="project_photo")
+    order = models.IntegerField()
+    
+    def __unicode__(self):
+      return self.file
